ef68d88a658eccc18da959fc8b6040a3a5de6c7c
tu3816329 <tu3816329@gmail.com>
2017-01-25 14:55:19 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//var express = require('express');
-//var fs = require('fs');
-//var app = express();
-//app.use(express.bodyParser());
-//app.post('/', function (request, response) {
-//    Console.log(request.body.name);
-//    response.writeHead(200, {'Content-Type': 'Application/Json'});
-//    response.end();
-//});
+
 var http = require('http');
 var url = require('url');
 var server = http.createServer(function (request, response) {
@@ -24,6 +16,12 @@ var server = http.createServer(function (request, response) {
         response.write(content);
         response.end();
     } else if (request.method === "POST") {
+        response.writeHeader(200, {'Content-type': "Application/json"});
+        var content = "";
+        content += "{'speech':'Please wait a moment for your order.','displayText':'Please wait a moment for your order.We are making it!'";
+        content += "'data':[],'contextOut':[],'source':[]";
+        response.write(content);
+        response.end();
         console.log("POST");
     }
 }).listen(process.env.PORT || 8080, function () {
